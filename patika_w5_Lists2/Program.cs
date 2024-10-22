781bed52f55e0ff869f449720b13848bacfe32fd
YasarDogann <yasardgn99@gmail.com>
2024-10-22 13:02:01 +0300

    comments

--- patika_w5_Lists2/Program.cs
+++ patika_w5_Lists2/Program.cs
@@ -4,15 +4,17 @@
     {
         static void Main(string[] args)
         {
+            // List ile dinamik liste oluşturdum.
             List<string> coffees = new List<string>();
 
             Console.WriteLine("Lütfen 5 adet Kahve giriniz");
 
+            // for döngüsü ile liste içine verileri kaydettim
             for(int i = 0; i< 5; i++)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Red; // yazı rengi kırmızı
                 Console.Write($"Kahve {i+1} : ");
-                Console.ResetColor();
+                Console.ResetColor(); // yazı rengini resetle
 
                 Console.ForegroundColor = ConsoleColor.Green;
                 coffees.Add(Console.ReadLine());
@@ -20,6 +22,7 @@
             }
 
             Console.WriteLine("\r\nGirilen Kahve İsimleri: ");
+            // foreach döngüsü ile listedeki elemanları çıktı verdim
             foreach(string coffee in coffees)
             {
                 Console.WriteLine(coffee);

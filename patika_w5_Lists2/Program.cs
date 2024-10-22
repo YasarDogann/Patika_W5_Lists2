fc8063bc9997c474fd04c37737a3abc5b30a41a2
YasarDogann <yasardgn99@gmail.com>
2024-10-22 12:42:31 +0300

    lists2

---
+++ patika_w5_Lists2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace patika_w5_Lists2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> coffees = new List<string>();
+
+            Console.WriteLine("Lütfen 5 adet Kahve giriniz");
+
+            for(int i = 0; i< 5; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"Kahve {i+1} : ");
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                coffees.Add(Console.ReadLine());
+                Console.ResetColor();
+            }
+
+            Console.WriteLine("\r\nGirilen Kahve İsimleri: ");
+            foreach(string coffee in coffees)
+            {
+                Console.WriteLine(coffee);
+            }
+
+            Console.Read();
+        }
+    }
+}
